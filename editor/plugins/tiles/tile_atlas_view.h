d8a41b2e05d813e7327ebff04d6ebfb90472ad75
SirQuartz <nicknhuelin@gmail.com>
2025-02-08 22:31:09 -0500

    formatting

--- editor/plugins/tiles/tile_atlas_view.h
+++ editor/plugins/tiles/tile_atlas_view.h
@@ -35,6 +35,7 @@
 #include "scene/gui/box_container.h"
 #include "scene/gui/button.h"
 #include "scene/gui/center_container.h"
+#include "scene/gui/color_picker.h"
 #include "scene/gui/label.h"
 #include "scene/gui/margin_container.h"
 #include "scene/resources/2d/tile_set.h"
